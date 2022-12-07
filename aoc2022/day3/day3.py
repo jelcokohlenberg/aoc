5e67d49e97965733ed77fe49b7c24177e3a56081
Jelco KÃöhlenberg <jelco.kohlenberg@gmail.com>
2022-12-07 11:01:13 +0100

    Update day 3 AOC

--- aoc2022/day3/day3.py
+++ aoc2022/day3/day3.py
@@ -1,71 +1,38 @@
-def part_1():
-    return
+def retrieve_letters():
+    total_list = []
+    for i in range(len(data)):
+        first = data[i][0:len(data[i])//2]
+        second = data[i][len(data[i])//2:]
+        for j in range(len(second)):
+            if second[j] in first:
+                total_list.append(second[j])
+                break
+    return total_list
 
-def most_common(data):
-    new_bit = ""
-    for digit in range(len(data[0])):
-        sum = 0
-        for bit in data:
-            sum += int(bit[digit])
-        if sum >= len(data)/2:
-            new_bit += "1"
+def group_badge():
+    total_list = []
+    i = 0
+    while i < len(data):
+        j = i + 1
+        k = i + 2
+        for a in data[i]:
+            if a in data[j] and a in data[k]:
+                total_list.append(a)
+                break
+        i += 3
+    return total_list
+
+def put_value(l_list):
+    total_sum = 0
+    for i in l_list:
+        addi = 0
+        if ord(i) - ord('a') >= 0:
+            addi = ord(i) - ord('a') + 1
         else:
-            new_bit += "0"
-    return new_bit
-
-
-def least_common(data):
-    new_bit = ""
-    for digit in range(len(data[0])):
-        sum = 0
-        for bit in data:
-            sum += int(bit[digit])
-        if sum < len(data) / 2:
-            new_bit += "1"
-        else:
-            new_bit += "0"
-    return new_bit
-
-
-def oxygen_generator(data):
-    for i in range(len(data[0])):
-        number = most_common(data)
-        yeet_list = []
-        for j in range(len(data)):
-            if data[j][i] != number[i]:
-                yeet_list.append(data[j])
-        for toyeet in yeet_list:
-            data.remove(toyeet)
-        if len(data) <= 1:
-            break
-    final = ""
-    for k in data:
-        final += k
-    return int(final, 2)
-
-def scrubber_generator(data):
-    for i in range(len(data[0])):
-        number = least_common(data)
-        yeet_list = []
-        for j in range(len(data)):
-            if data[j][i] != number[i]:
-                yeet_list.append(data[j])
-        for toyeet in yeet_list:
-            data.remove(toyeet)
-        if len(data) <= 1:
-            break
-    final = ""
-    for k in data:
-        final += k
-    return int(final, 2)
-
-
-
-def part_2():
-
-    return
-
-
+            addi = ord(i) - ord('A') + 27
+        print(addi)
+        total_sum += addi
+    return total_sum
 
 def read_file():
     anything = []
@@ -76,12 +43,7 @@ def read_file():
 
 
 if __name__ == '__main__':
-    data1 = read_file()
-    data2 = read_file()
-    a = scrubber_generator(data1)
-    b = oxygen_generator(data2)
-    print(a)
-    print(b)
-    print(a * b)
-    # part 1
+    data = read_file()
+    print(group_badge())
+    print(put_value(group_badge()))
     #print(part_2())
